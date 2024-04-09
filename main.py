9e15004f4c782e487f1ad1b1371e35f981a4e3f8
Cmon-Tars <jpadilla@techsoup.org>
2024-04-09 10:04:41 -0400

    finished the tool

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 
+def sort_on(dict):
+    return dict["number"]
+
 def main():
     with open('books/Frankenstein.txt', 'r') as f:
         data = f.read()
-    print(data)
+        data = data.lower()
+    words = data.split()
+    chars = [c for c in data]
+    countsForReport = []
+    for char in set(chars):
+        if char.isalpha():
+            counts = {}
+            matches = [x for x in chars if x == char]
+            counts["char"] = char
+            counts["number"] = len(matches)
+            countsForReport.append(counts)
+    countsForReport.sort(reverse=True, key = sort_on)
+
+    message = f"--- Begin report of books/frankenstein.txt --- \n{len(words)} words found in the document \n"
+    for i in countsForReport:
+        message += f"\nThe '{i["char"]}' character was found {i["number"]} times"
+    message += "\n--- End report ---"
+    print(message)
+
 
 if __name__ == "__main__":
     main()
